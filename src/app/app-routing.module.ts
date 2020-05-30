2b5ed444e393c5a5c8adf906a5c148eb38d5232b
alejo120792 <alejo120792120792@hotmail.com>
2020-05-30 17:52:30 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LoginComponent } from './login/login.component';
+import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
+import { RegisterComponent } from './login/register.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: 'progress',  component: ProgressComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+  { path: 'login',     component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**',        component: NopagesfoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
