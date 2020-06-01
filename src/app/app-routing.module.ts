c476447d19651f1cd5a0d44c88f37f4e7a4cb969
alejo120792 <alejo120792120792@hotmail.com>
2020-05-31 19:01:41 -0500

    Modificaciones en los modulos y routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
 import { LoginComponent } from './login/login.component';
-import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
 
 
-const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'graficas1', component: Graficas1Component },
-      { path: 'progress',  component: ProgressComponent },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
-  { path: 'login',     component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '**',        component: NopagesfoundComponent }
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NopagesfoundComponent }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
